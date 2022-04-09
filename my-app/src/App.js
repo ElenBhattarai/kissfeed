5dcb42f14b47e28836fa4cc9359a77adc9f96698
Heminway <r673h066@e1434-020faj1gn.home.ku.edu>
2022-04-09 04:14:43 -0500

    Fixed margins and addedview article fstudfdff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,48 @@ import './App.css';
 
 import {useState} from 'react';
 function App() {
-  const [followed, setFollowed] = useState(false)
+  let data;
+  const [followed, setFollowed] = useState(true)
+  const [request,setrequest] = useState(false)
+  const [clicked, setClicked] = useState(true)
+  const [body, setBody] = useState(false)
+
+  const handleClick = ()=> {
+    setClicked(false)
+    setBody(true)
+  }
+
+  const handleClose = ()=> {
+    setClicked(true)
+    setBody(false)
+  }
+
+
+  // const fetchapi = async () => {
+  //   const res = await fetch("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c8503c90442c4249860c2ead0a8ce1f8")
+  //   data = await res.json();
+  //   // console.log(data.articles)
+  //   setrequest(true)
+  //   apiwork()
+  // }
+
+  // fetchapi()
+  
+  // const apiwork = () => {
+  //   // for (let da of data.articles) {
+  //   //   // console.log(da.title)
+  //   //   console.log(da.content)
+  //   // }
+
+  //   console.log(data.articles[0].content)
+  // }
+
+  
+  
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -29,17 +70,50 @@ function App() {
                 )}
                 
               </div>
+              
+              <div id = 'row1-custom'>
+                <button>
+                Custom
+                </button>
+              </div>
+
+
           </div>
           <div id = 'column2'>
             <div> 
               <div id = 'newsrow'> 
                 News Title
               </div>
-              <div id = 'newsrowtext'>
-              CNN was given exclusive access to the power plant for the first time since it came back into Ukrainian control.
 
-Officials at the plant explain the levels inside the room used by Russian soldiers are only slightly above what the World Nuclear Association describes as naturally occurring radiation. One-time contact would not be dangerous but continuous exposure would pose a health hazard.
-              </div> 
+              {clicked && (
+                <div id = 'newsrowtext'>
+                  dkasjdsakjdabdasnsdbsajhdqhbdnabdsjhdahbdmnbdajshbdalbdwandsahdahdlndbasldbsahjdasdbsadlhjasdjashbdlasdbahdwandadbahd 
+                  <button onClick = {handleClick}>View the article</button>
+                </div>
+                
+              )}
+              {body && (
+                <div id = 'newsrowtext'>
+                  <button onClick = {handleClose}>Close Article</button>
+                   jdsajdsakdsadbasdjaddjasbd
+                   dkasjdsakjdabdasnsdbsajhdqhbdnabdsjhdahbdmnbdajshbdalbdwandsahdahdlndbasldbsahjdasdbsadlhjasdjashbdlasdbahdwandadbahddas
+                   asmdnas
+                   asmdnasd
+                   sadnasd
+                   dkasjdsakjdabdasnsdbsajhdqhbdnabdsjhdahbdmnbdajshbdalbdwandsahdahdlndbasldbsahjdasdbsadlhjasdjashbdlasdbahdwandadbahdm
+                   dkasjdsakjdabdasnsdbsajhdqhbdnabdsjhdahbdmnbdajshbdalbdwandsahdahdlndbasldbsahjdasdbsadlhjasdjashbdlasdbahdwandadbahd
+
+                   asldnasd
+                   dkasjdsakjdabdasnsdbsajhdqhbdnabdsjhdahbdmnbdajshbdalbdwandsahdahdlndbasldbsahjdasdbsadlhjasdjashbdlasdbahdwandadbahdsadmna
+                   dkasjdsakjdabdasnsdbsajhdqhbdnabdsjhdahbdmnbdajshbdalbdwandsahdahdlndbasldbsahjdasdbsadlhjasdjashbdlasdbahdwandadbahddsa,
+                   as,dad
+                </div>
+              )
+              }
+              
+
+
+              
             </div>
           </div>
         </div>
