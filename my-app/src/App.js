96a36243dc5997f74aeb5e34f448d734c913b56b
Heminway <r673h066@e1434-020faj1gn.home.ku.edu>
2022-04-09 01:04:00 -0500

    some changes idk

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {useState} from 'react';
 function App() {
+  const [followed, setFollowed] = useState(false)
   return (
     <div className="App">
       <header className="App-header">
@@ -18,13 +20,27 @@ function App() {
                 </button>
               </div>
               <div id = 'row1'>
+                {followed && (
+
                 <button>
                 Followed
                 </button>
+
+                )}
+                
               </div>
           </div>
           <div id = 'column2'>
-            dsahkajhd
+            <div> 
+              <div id = 'newsrow'> 
+                News Title
+              </div>
+              <div id = 'newsrowtext'>
+              CNN was given exclusive access to the power plant for the first time since it came back into Ukrainian control.
+
+Officials at the plant explain the levels inside the room used by Russian soldiers are only slightly above what the World Nuclear Association describes as naturally occurring radiation. One-time contact would not be dangerous but continuous exposure would pose a health hazard.
+              </div> 
+            </div>
           </div>
         </div>
       </body>
