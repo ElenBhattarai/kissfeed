4380295739f321aca0baaf355f61a17e83a12ee7
Minwoo Lee <leejoseph892@gmail.com>
2022-04-09 07:00:14 -0500

    parser

--- server/views/rss.js
+++ server/views/rss.js
@@ -1,8 +1,9 @@
 
-const fetchapi = async () => {
-    const res = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2022-03-09&sortBy=publishedAt&apiKey=55463d78ac65493fa8a2fa395767889e")
-    let data = await res.json()
-    console.log(data)
-}
+var parser = require('article-parser')
+const url = 'https://www.nbcnews.com/news/world/live-blog/russia-ukraine-war-live-updates-moscow-condemned-train-station-strike-rcna23710'
 
-fetchapi()
+parser.extract(url).then((article) => {
+    console.log(article)
+    }).catch((err) => {
+    console.trace(err)
+})
